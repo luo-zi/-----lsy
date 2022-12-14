05cb7c5649ab14e4cab3c1bbc32e5bbbfee6d0f1
luozi <zjw1355528158@outlook.com>
2022-12-14 23:32:19 +0800

    增加页面背景色 格式化所有代码

--- 首页/js/lunbo.js
+++ 首页/js/lunbo.js
@@ -1,29 +1,29 @@
-currentSelected=0;
-$(function (){
-    $(".lunbo-button-item").hide();
-    $(".lunbo-button-item").click(function(){
-        currentSelected=$(this).attr("num");
-        $(".lunbo-button-item").css("background-color","transparent");;
-        $(".lunbo-button-item").css("boder",".1rem solid #fff");
-        $(this).css("background-color","#fff");
-        $(this).css("boder",".1rem solid #fff");
-        $(".lunbo-list").removeClass("img1");
-        $(".lunbo-list").removeClass("img2");
-        $(".lunbo-list").removeClass("img3");
-        $(".lunbo-list").addClass("img"+currentSelected);
-    });
-    
-     $(".lunbo").mouseleave(function () { 
-        $(".lunbo-button-item").hide();
-        $(".lunbo-button-item").css("background-color","transparent");;
-        $(".lunbo-button-item").css("boder",".1rem solid #fff");
-        $(".lunbo-list").removeClass("img1");
-        $(".lunbo-list").removeClass("img2");
-        $(".lunbo-list").removeClass("img3");
-        $(".lunbo-list").addClass("lunbo-list-animation");
-     });
-     $(".lunbo").mouseenter(function () { 
-        $(".lunbo-button-item").show();
-       
-     });
+currentSelected = 0;
+$(function () {
+   $(".lunbo-button-item").hide();
+   $(".lunbo-button-item").click(function () {
+      currentSelected = $(this).attr("num");
+      $(".lunbo-button-item").css("background-color", "transparent");;
+      $(".lunbo-button-item").css("boder", ".1rem solid #fff");
+      $(this).css("background-color", "#fff");
+      $(this).css("boder", ".1rem solid #fff");
+      $(".lunbo-list").removeClass("img1");
+      $(".lunbo-list").removeClass("img2");
+      $(".lunbo-list").removeClass("img3");
+      $(".lunbo-list").addClass("img" + currentSelected);
+   });
+
+   $(".lunbo").mouseleave(function () {
+      $(".lunbo-button-item").hide();
+      $(".lunbo-button-item").css("background-color", "transparent");;
+      $(".lunbo-button-item").css("boder", ".1rem solid #fff");
+      $(".lunbo-list").removeClass("img1");
+      $(".lunbo-list").removeClass("img2");
+      $(".lunbo-list").removeClass("img3");
+      $(".lunbo-list").addClass("lunbo-list-animation");
+   });
+   $(".lunbo").mouseenter(function () {
+      $(".lunbo-button-item").show();
+
+   });
 });
